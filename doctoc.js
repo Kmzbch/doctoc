7b3d0e93c1f7bc2f1d3068c533a566c5a8e7fcca
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2019-04-05 21:58:50 -0400

    deleted unnecessary part

--- doctoc.js
+++ doctoc.js
@@ -7,13 +7,6 @@ var path = require('path'),
   transform = require('./lib/transform'),
   files;
 
-function cleanPath(path) {
-  var homeExpanded = (path.indexOf('~') === 0) ? process.env.HOME + path.substr(1) : path;
-
-  // Escape all spaces
-  return homeExpanded.replace(/\s/g, '\\ ');
-}
-
 function transformAndSave(files, mode, maxHeaderLevel, title, notitle, entryPrefix, stdOut) {
   console.log('\n==================\n');
 
@@ -53,77 +46,6 @@ function transformAndSave(files, mode, maxHeaderLevel, title, notitle, entryPref
       console.log(x.data);
     }
   });
-}
-
-function printUsageAndExit(isErr) {
-
-  var outputFunc = isErr ? console.error : console.info;
-
-  outputFunc('Usage: doctoc [mode] [--entryprefix prefix] [--notitle | --title title] [--maxlevel level] <path> (where path is some path to a directory (e.g., .) or a file (e.g., README.md))');
-  outputFunc('\nAvailable modes are:');
-  for (var key in modes) {
-    outputFunc('  --%s\t%s', key, modes[key]);
-  }
-  outputFunc('Defaults to \'' + mode + '\'.');
-
-  process.exit(isErr ? 2 : 0);
-}
-
-var modes = {
-  bitbucket: 'bitbucket.org',
-  nodejs: 'nodejs.org',
-  github: 'github.com',
-  gitlab: 'gitlab.com',
-  ghost: 'ghost.org'
-}
-
-var mode = modes['github'];
-
-var argv = minimist(process.argv.slice(2), {
-  boolean: ['h', 'help', 'T', 'notitle', 's', 'stdout'].concat(Object.keys(modes)),
-  string: ['title', 't', 'maxlevel', 'm', 'entryprefix'],
-  unknown: function (a) {
-    return (a[0] == '-' ? (console.error('Unknown option(s): ' + a), printUsageAndExit(true)) : true);
-  }
-});
-
-if (argv.h || argv.help) {
-  printUsageAndExit();
-}
-
-for (var key in modes) {
-  if (argv[key]) {
-    mode = modes[key];
-  }
-}
-
-var title = argv.t || argv.title;
-var notitle = argv.T || argv.notitle;
-var entryPrefix = argv.entryprefix || '-';
-var stdOut = argv.s || argv.stdout
-
-var maxHeaderLevel = argv.m || argv.maxlevel;
-if (maxHeaderLevel && isNaN(maxHeaderLevel) || maxHeaderLevel < 0) {
-  console.error('Max. heading level specified is not a positive number: ' + maxHeaderLevel), printUsageAndExit(true);
-}
-
-for (var i = 0; i < argv._.length; i++) {
-  var target = cleanPath(argv._[i]),
-    stat = fs.statSync(target)
-
-  if (stat.isDirectory()) {
-    console.log('\nDocToccing "%s" and its sub directories for %s.', target, mode);
-    files = file.findMarkdownFiles(target);
-  } else {
-    console.log('\nDocToccing single file "%s" for %s.', target, mode);
-    files = [{
-      path: target
-    }];
-  }
-
-  transformAndSave(files, mode, maxHeaderLevel, title, notitle, entryPrefix, stdOut);
-
-  console.log('\nEverything is OK.');
 }
 
 function dummyFunc() {
